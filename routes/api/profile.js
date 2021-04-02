06fcc243ed23a10a6105b03e9b0cedbdc7e7fc76
saoudi nader <saoudi.nader7@gmail.com>
2021-04-02 14:19:42 +0100

    Profile create update

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+const validateProfileInput = require('../../Validation/profile');
 
 
 //Load Profile Model 
@@ -20,6 +21,7 @@ router.get('/',passport.authenticate('jwt',{ session: false }),(req,res) => {
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+    .populate('user',['name', 'avatar'])
     .then(profile => {
         if(!profile){
             errors.noprofile = 'There is no profile for this user';
@@ -35,11 +37,16 @@ router.get('/',passport.authenticate('jwt',{ session: false }),(req,res) => {
 //@desc  Create or Edit user profile
 //@access private
 router.post('/',passport.authenticate('jwt',{ session: false }),(req,res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+    //check validation
+    if (!isValid){
+        return res.status(400).json(errors);
+    }
     //Get fields 
     const profileFields = {};   
     profileFields.user = req.user.id;
     if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.campany) profileFields.campany = req.body.campany;
+    if (req.body.company) profileFields.company = req.body.company;
     if (req.body.website) profileFields.website = req.body.website;
     if (req.body.location) profileFields.location = req.body.location;
     if (req.body.bio) profileFields.bio = req.body.bio;
@@ -51,17 +58,17 @@ router.post('/',passport.authenticate('jwt',{ session: false }),(req,res) => {
     } 
 
     profileFields.social = {};
-    if (req.body.youtube) profileFields.social = req.body.youtube;
-    if (req.body.facebook) profileFields.social = req.body.facebook;
-    if (req.body.twitter) profileFields.social = req.body.twitter;
-    if (req.body.linkedin) profileFields.social = req.body.linkedin;
-    if (req.body.instagram) profileFields.social = req.body.instagram;
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
     Profile.findOne({user : req.user.id})
         .then(profile => {
             if(profile) {
                 //Update
-                Profile.findByIdAndUpdate({ user :req.user.id }, { $set: profileFields }, {new: true})
+                Profile.findOneAndUpdate({ user :req.user.id }, { $set: profileFields }, {new: true})
                 .then(profile => res.json(profile));
             }else{
                 //Create
@@ -78,6 +85,7 @@ router.post('/',passport.authenticate('jwt',{ session: false }),(req,res) => {
                 }) 
             }
         })
+    
 });
 
 
